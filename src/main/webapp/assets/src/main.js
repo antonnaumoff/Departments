e066cad9ab697974593953db4bce22e93eeae928
antonnaumoff <antonnaumoff@gmail.com>
2016-09-19 18:36:21 +0300

    Ext by cdn without css

--- src/main/webapp/assets/src/main.js
+++ src/main/webapp/assets/src/main.js
@@ -1,5 +1,24 @@
-function hello () {
-    alert('Hello World');
-}
+// require ('ext-core.js');
 
-hello();
+
+Ext.create('Ext.panel.Panel', {
+    renderTo: Ext.getBody(),
+    width: 400,
+    height: 200,
+    title: 'Container Panel',
+    layout: 'column',
+    items: [
+        {
+            xtype: 'panel',
+            title: 'Child Panel 1',
+            height: 100,
+            columnWidth: 0.5
+        },
+        {
+            xtype: 'panel',
+            title: 'Child Panel 2',
+            height: 100,
+            columnWidth: 0.5
+        }
+    ]
+});
